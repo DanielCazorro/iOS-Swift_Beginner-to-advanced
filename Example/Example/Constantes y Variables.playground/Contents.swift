fbc96f646164128d21abedded70f0941a1ef2201
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-09 11:47:43 +0100

    Variables and constants

---
+++ Example/Example/Constantes y Variables.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+// Esto es una variable, y se escribe con var
+var str = "Hello, playground"
+str = "Hola Mundo"
+
+// Esto es una constante, y se escribe con let
+let stringConstant = "Hola mundo cruel"
+let myFullName = "Daniel"
