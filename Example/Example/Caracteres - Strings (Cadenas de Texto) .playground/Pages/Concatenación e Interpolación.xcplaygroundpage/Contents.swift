38908de7ad371d15ba1334e80900fe79365d64d6
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-09 17:22:51 +0100

    Concatenación e interpolación

---
+++ Example/Example/Caracteres - Strings (Cadenas de Texto) .playground/Pages/Concatenación e Interpolación.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// Concatenación
+var myString = "Bienvenidos" + " " + "al curso de Swift"
+myString += "\n¿Os está gustando la clase?"
+
+let name = "Daniel"
+let country = "España"
+let myPersonalInfo = "Mi nombre es " + name + " y vivo en " + country
+
+// Interpolación
+let myPersonalInfoWithInterpolation = "Mi nombre es \(name) y vivo en \(country) "
+
+//: [Next](@next)
