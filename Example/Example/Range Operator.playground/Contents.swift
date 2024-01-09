d6e00cff7a7063fb04e2b92e6b4b92ca9a8896bd
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-09 21:59:16 +0100

    Range Operator

---
+++ Example/Example/Range Operator.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var arregloEjemplo = [3,112,2,3,4,5,65,8,8,9]
+
+// Operador rango doble lado / two-sided range operator
+
+var subsetArreglo = arregloEjemplo[1...3]
+
+// Operador rango lado único / one-sided range operator
+var subSetArreglo2 = arregloEjemplo[5...]
+
+
+var rangoCerrado = [1...3]
+//1.2.3.4.5
+// var subArray = Array(rangoCerrado[0])
+// subArray[2]
