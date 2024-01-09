331fdede0279da8de0630a49f071f5d787ad2d41
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-09 22:36:15 +0100

    While

---
+++ Example/Example/While.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+// Bucle While
+
+var age = 10
+
+while age < 18 {
+    print("Eres menor de edad. Tienes \(age) años. Te faltan \(18 - age) años")
+    age += 1
+}
+
+print("Ya eres mayor de edad")
