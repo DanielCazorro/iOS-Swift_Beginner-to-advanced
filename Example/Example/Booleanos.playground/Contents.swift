38b540b28b291d07331f695c092d1c592901b706
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-09 19:53:48 +0100

    Booleans

---
+++ Example/Example/Booleanos.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var estudiosJuanVillalvazo = "Grado Universitario"
+var salario = 0
+var cedulaProfesional = true
+var cedulaMaestria = false
+
+if cedulaProfesional {
+    salario = 9000
+} else {
+    salario = 8000
+}
+
+salario
