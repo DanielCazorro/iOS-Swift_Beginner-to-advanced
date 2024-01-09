cd6f70446c6d9114bcefb2fbc5febe4e603c83b2
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-09 22:07:51 +0100

    Operators

---
+++ Example/Example/Estructuras de Control.playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+// Operador de asignación =
+
+let numeroConstante = 18
+
+// Operadores Aritméticos + - * /
+
+var numeroVariable = 2
+
+numeroConstante + numeroVariable
+numeroConstante - numeroVariable
+numeroConstante * numeroVariable
+numeroConstante / numeroVariable
+
+// Operadores Compuestos
+
+// numeroVariable = numeroVariable + 2
+numeroVariable += 2
+numeroVariable -= 2
+numeroVariable *= 2
+numeroVariable /= 2
+
+// Operadores de comparación
+var x = 1
+var y = 2
+
+// Igual a ==
+
+x == y
+
+// Diferente a !=
+
+x != y
+
+// Mayor que >
+
+x > y
+
+// Menor que <
+
+x < y
+
+// Mayor o igual que >=
+
+x >= y
+
+// Menor o igaul que <=
+
+x <= y
