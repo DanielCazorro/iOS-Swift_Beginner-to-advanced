cc1cde23e633f0e423c5876268a5e5477d22c909
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-09 11:41:14 +0100

    Simple comments

--- Example/Example/MyPlayground.playground/Contents.swift
+++ Example/Example/MyPlayground.playground/Contents.swift
@@ -1,3 +1,10 @@
+/* 
+Import sirve para importar liberías
+Una librería es una colección de líneas de código que podemos usar para no reinventar la rueda
+*/
 import UIKit
 
+// Esto es un comentario
+
 var str = "Hi, playground"
+
