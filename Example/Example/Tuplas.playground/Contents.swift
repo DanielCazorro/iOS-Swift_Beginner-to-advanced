b8a1d15eeb93c209265ff75c951421fc8a502960
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-09 20:41:24 +0100

    Tuplas

---
+++ Example/Example/Tuplas.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+/*
+            // Nombre, apellido, edad, estatura
+var persona = ("Juan", "Rodriguez", 30, 1.86)
+
+persona.0
+
+var (nombre, apellido, edad, estatura) = persona
+
+nombre
+*/
+
+var personaNamedTuples = (nombre:"Juan", apellido:"Rodriguez", edad:30, estatura:1.86)
+
+personaNamedTuples.nombre
