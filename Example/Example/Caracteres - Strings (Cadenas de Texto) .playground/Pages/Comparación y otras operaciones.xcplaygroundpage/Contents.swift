121c3e3d2c8748144d44a3aa4fa09c0ce1fbcaa3
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-09 17:27:14 +0100

    Comparing and other operations

---
+++ Example/Example/Caracteres - Strings (Cadenas de Texto) .playground/Pages/Comparación y otras operaciones.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// Comparación
+let name1 = "Daniel"
+let name2 = "Vicente"
+let name3 = "Daniel"
+
+// No son iguales
+name1 == name2
+
+// Son iguales
+name1 == name3
+
+// Contenido
+let myContent = "Mi nombre es Daniel"
+
+myContent.contains(name1)
+
+// Vacía
+let myEmptyString = ""
+
+myEmptyString == ""
+myEmptyString.isEmpty
+
+// Recorrido
+for valor in name1 {
+    print(valor)
+}
+
+
+//: [Next](@next)
