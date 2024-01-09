387d54f211ff4a7e25e452d71b47360a0c583f16
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-09 12:11:29 +0100

    Integers and decimals

---
+++ Example/Example/Tipos de Datos.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+// Enteros
+var x:Int8 = 18
+var z:Int = 20000000
+
+// Flotantes
+var y:Double = 18.8
+
+// Operadores aritméticos + - * /
+var sumaEnteros = x + 2
+
+var sumaFlotantes = y + 2.2
+
+var suma = Double(x) + Double(y)
+
+var division = x/2
+
+var divisionDecimal = Double(x) / 5
+
+// Caracteres - Strings (Cadenas de Texto)
