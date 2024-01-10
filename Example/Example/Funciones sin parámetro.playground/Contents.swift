fa9e9624a28521103c2cea9caf3f3a274f5f80ca
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-10 08:58:01 +0100

    Functions without parameter

---
+++ Example/Example/Funciones sin parámetro.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+/*
+ var numeroAleatorio:Int
+ numeroAleatorio = Int(arc4random_uniform(100))
+ */
+
+func holaMundo() {
+    print("Hola mundo")
+}
+
+holaMundo()
+
+func sumaDosNumerosCualquiera(){
+
+    var numero = 2 + 2
+    
+}
+
+sumaDosNumerosCualquiera()
