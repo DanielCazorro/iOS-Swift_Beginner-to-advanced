879e2f94502ccb1910ee3af1738b49267d9729bc
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-10 09:07:45 +0100

    functions with multiple parameters

--- Example/Example/Funciones sin parámetro.playground/Contents.swift
+++ Example/Example/Funciones sin parámetro.playground/Contents.swift
@@ -29,3 +29,19 @@ func cuadradoDe(numero:Int) {
 // cuadradoDe(numero: 10)
 
 
+func cuboDe(numero:Int) -> Int{
+    
+    let numeroCubo = numero * numero * numero
+    return numeroCubo
+}
+
+_ = cuboDe(numero: 10)
+
+
+func saludarAlumno(nombre:String, mensaje:String){
+    
+    print("Hola \(nombre) \(mensaje)")
+    
+}
+
+saludarAlumno(nombre: "Maria", mensaje: "te damos la bienvenida al curso de iOS con Swift")
