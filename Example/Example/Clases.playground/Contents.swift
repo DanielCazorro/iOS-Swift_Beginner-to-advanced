2b3d1fe15f34d295354175e52cf588ada9dd7607
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-10 10:05:01 +0100

    Clases: Referencia / Struct: Valor

--- Example/Example/Clases.playground/Contents.swift
+++ Example/Example/Clases.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class automovil {
+class claseAutomovil {
     
     var color = "neutro"
     var numeroLlantas = 4
@@ -20,10 +20,47 @@ class automovil {
     
 }
 
-var miObjetoMazda = automovil()
+var miObjetoMazda = claseAutomovil()
 
 miObjetoMazda.precio
 
 miObjetoMazda.precio = 5000
 
 print(miObjetoMazda.precio)
+
+struct estructuraAutomovil {
+    var color = "neutro"
+    var numeroLlantas = 4
+    var precio = 0
+    
+    func encender() -> Bool {
+        return true
+    }
+    
+    func apagar() -> Bool {
+        return true
+    }
+    
+    func acelerar() -> Bool {
+        return true
+    }
+}
+
+var miEstructuraMazda = estructuraAutomovil()
+
+miEstructuraMazda.precio
+miEstructuraMazda.precio = 5000
+
+print(miEstructuraMazda.precio)
+
+var miSegundoObjetoMazda = miObjetoMazda
+var miSegundaEstructuraMazda = miEstructuraMazda
+
+miSegundoObjetoMazda.precio = 6000
+miSegundaEstructuraMazda.precio = 8000
+
+print(miSegundoObjetoMazda.precio)
+print(miSegundaEstructuraMazda.precio)
+
+print(miObjetoMazda.precio)
+print(miEstructuraMazda.precio)
