7485dfbddc1ccb5d0bb8d985b391b542023a1412
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-10 09:21:20 +0100

    Closures

---
+++ Example/Example/Closures.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+/* Closures
+ 
+ {
+
+ (parametros) -> tipo-valor-retorno in
+ codigo
+ 
+ }
+ 
+ */
+
+let miPrimerClosure = {(materiaUno:Double, materiaDos:Double, materiaTres:Double) -> Double in
+        return (materiaUno + materiaDos + materiaTres)/3
+}
+
+miPrimerClosure(100, 22, 88)
+
+
+print("Mi calificación promedio fue de \(miPrimerClosure(100, 22, 88))")
