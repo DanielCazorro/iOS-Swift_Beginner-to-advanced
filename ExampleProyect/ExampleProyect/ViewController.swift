36ce6a047fe12fc2f68f85a076a8069c398db50f
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-11 11:32:26 +0100

    Simple autolayout

--- ExampleProyect/ExampleProyect/ViewController.swift
+++ ExampleProyect/ExampleProyect/ViewController.swift
@@ -14,7 +14,11 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         etiqueta.text = "Ya he cambiado"
+        etiqueta.text = "Ya he cambiado por segunda vez"
+        etiqueta.text = "Ya he cambiado por tercera vez"
+        etiqueta.text = "Ya he cambiado por cuarta vez"
     }
     
     @IBAction func cambiarTexto(_ sender: Any) {
