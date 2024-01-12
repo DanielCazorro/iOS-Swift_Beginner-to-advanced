03f156d23fff05a8e87510587f0c5a7bdb4d750b
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-12 16:57:04 +0100

    Class Automovil and Views

---
+++ FabricaAutomóviles/FabricaAutomóviles/Automovil.swift
@@ -0,0 +1,23 @@
+//
+//  Automovil.swift
+//  FabricaAutomóviles
+//
+//  Created by Daniel Cazorro Frias  on 12/1/24.
+//
+
+import Foundation
+
+class Automovil {
+    
+    var color = "neutro"
+    var numeroLlantas = 4
+    var precio = 0
+    
+    func encender() -> Bool {
+        return true
+    }
+    
+    func apagar() -> Bool {
+        return false
+    }
+}
