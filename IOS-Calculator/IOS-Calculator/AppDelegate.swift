1215c679adcfdd7091e5d2f086294b96c5185277
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-13 22:10:06 +0100

    Initial commit: iOS Calculator

---
+++ IOS-Calculator/IOS-Calculator/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  IOS-Calculator
+//
+//  Created by Daniel Cazorro Frias  on 13/1/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
