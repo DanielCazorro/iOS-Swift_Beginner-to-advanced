859875c6c3a8b8931fa175f3d4b570589d683261
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-13 22:45:47 +0100

    Main View: XIB

---
+++ IOS-Calculator/IOS-Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  IOS-Calculator
+//
+//  Created by Daniel Cazorro Frias  on 13/1/24.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    // MARK:  Inicialización
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK:  Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
