f876b2309e011ad07c3a5c35209f3f3f1aa18015
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-14 16:15:54 +0100

    Initial commit: Alamofire instalation

---
+++ Networking/Networking/NetworkingProvider.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkingProvider.swift
+//  Networking
+//
+//  Created by Daniel Cazorro Frias  on 14/1/24.
+//
+
+import Foundation
+import Alamofire
+
+final class NetworkingProvider {
+    
+    static let shared = NetworkingProvider()
+}
---
+++ Networking/Networking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Daniel Cazorro Frias  on 14/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+}
+
