0626704e7228f3de00bbfda175fdccaea839d821
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-14 19:22:41 +0100

    Stacks: ZStack, HStack and VStack

--- SwiftUITutorial/SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/SwiftUITutorial/ContentView.swift
@@ -9,16 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("¡Hola mundo!")
-                .foregroundColor(Color.orange)
-                .font(.title)
+        
+        VStack(alignment: .trailing, spacing: 20) {
+            Text("Welcome to Jurassic World")
+                .font(.largeTitle)
+                .foregroundStyle(.blue)
                 .padding()
-        }
-        .padding()
+                .background(Color.gray)
+            
+            Spacer()
+            
+            ZStack {
+                Text("")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.gray).padding()
+                Text("")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.green).padding(40)
+                Text("")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.red).padding(60)
+                Text("")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.blue).padding(80)
+            }
+            
+            HStack {
+                Text("Hola mundo")
+                    .padding()
+                    .background(Color.red)
+                
+                Spacer()
+                
+                Text("Gomu gomu no")
+                    .padding()
+                    .background(Color.orange)
+            }
+            
+            Text("6")
+            Text("7")
+            Text("8")
+            Text("9")
+            Text("10")
+            
+        }.background(Color.yellow)
     }
 }
 
