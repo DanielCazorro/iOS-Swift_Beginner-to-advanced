2a4b37fb82af65f8af43a1c58869af6db2fc9ca1
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-14 22:33:32 +0100

    SwiftUI: Navigation between screens

---
+++ SwiftUITutorial/SwiftUITutorial/Views/ListDetailView.swift
@@ -0,0 +1,33 @@
+//
+//  ListDetailView.swift
+//  SwiftUITutorial
+//
+//  Created by Daniel Cazorro Frias  on 14/1/24.
+//
+
+import SwiftUI
+
+struct ListDetailView: View {
+    
+    var programmer: Programmer
+    
+    var body: some View {
+        VStack {
+            programmer.avatar
+                .resizable()
+                .frame(width: 200, height: 200)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.black, lineWidth: 4))
+                .shadow(color: Color.gray, radius: 5)
+            Text(programmer.name)
+                .font(.largeTitle)
+            Text(programmer.languages)
+                .font(.title)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    ListDetailView(programmer: Programmer(id: 0, name: "Daniel", languages: "Swift", avatar: Image(systemName: "person.fill")))
+}
--- SwiftUITutorial/SwiftUITutorial/Views/ListView.swift
+++ SwiftUITutorial/SwiftUITutorial/Views/ListView.swift
@@ -8,18 +8,22 @@
 import SwiftUI
 
 let programmers: [Programmer] = [
-    Programmer(id: 0, name: "Daniel", languages: "Swift", avatar: Image(systemName: "person.fill")),
+    Programmer(id: 0, name: "Daniel", languages: "Swift", avatar: Image(systemName: "person")),
     Programmer(id: 1, name: "Ana", languages: "Python", avatar: Image(systemName: "person.fill")),
-    Programmer(id: 2, name: "Carlos", languages: "JavaScript", avatar: Image(systemName: "person.fill")),
-    Programmer(id: 3, name: "Laura", languages: "Java", avatar: Image(systemName: "person.fill")),
-    Programmer(id: 4, name: "Diego", languages: "C#", avatar: Image(systemName: "person.fill")),
-    Programmer(id: 5, name: "Elena", languages: "Ruby", avatar: Image(systemName: "person.fill"))
+    Programmer(id: 2, name: "Carlos", languages: "JavaScript", avatar: Image(systemName: "face.smiling.fill")),
+    Programmer(id: 3, name: "Laura", languages: "Java", avatar: Image(systemName: "person.2.fill")),
+    Programmer(id: 4, name: "Diego", languages: "C#", avatar: Image(systemName: "person.circle.fill")),
+    Programmer(id: 5, name: "Elena", languages: "Ruby", avatar: Image(systemName: "face.smiling"))
 ]
 
 struct ListView: View {
     var body: some View {
-        List(programmers, id: \.id) { programmer in
-            RowView(programmer: programmer)
+        NavigationView {
+            List(programmers, id: \.id) { programmer in
+                NavigationLink(destination: ListDetailView(programmer: programmer)) {
+                    RowView(programmer: programmer)
+                }
+            }.navigationTitle("Programmers")
         }
     }
 }
