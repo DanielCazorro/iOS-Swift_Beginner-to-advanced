21bee07d588a6c181a589bc000d38df0f3567475
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-15 09:09:33 +0100

    Initial commit: Firebase app

---
+++ FirebaseTutorial/FirebaseTutorial/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FirebaseTutorial
+//
+//  Created by Daniel Cazorro Frias  on 15/1/24.
+//
+
+import UIKit
+import FirebaseAnalytics
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Analytics Event
+        Analytics.logEvent("InitScreen", parameters: ["message":"Integración de Firebase Completa"])
+    }
+
+
+}
+
